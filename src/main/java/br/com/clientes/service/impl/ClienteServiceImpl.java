433d257a3520256b883c50b6945966bec1d46462
Gustavo Biasi <gustavo.biasi@franqueadolinx.com.br>
2022-10-07 11:01:30 -0300

    testando nova branch

--- src/main/java/br/com/clientes/service/impl/ClienteServiceImpl.java
+++ src/main/java/br/com/clientes/service/impl/ClienteServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class ClienteServiceImpl implements ClienteService {
 
+
     @Autowired
     private ClienteRepository clienteRepository;
 
